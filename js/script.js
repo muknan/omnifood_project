d0ad3ae3b82aa03239b3c376f5348be8a6b2614d
muknan <muk.msi@gmail.com>
2023-07-26 16:04:28 +0530

    Making smooth scrolling work using js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,41 @@ console.log(h1);
 //   h1.style.padding = "5rem";
 // });
 
+//Set current year in footer
 const yearEl = document.querySelector(".footer-year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+////////////////////////////////////////////////////////////
+//Implementing smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    //Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
